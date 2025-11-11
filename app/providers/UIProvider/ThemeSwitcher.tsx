1502a3c474d4da6ef049c200a150c60e795b3586
Hernán Gonzalez <hernan.gonzalez.magliano@gmail.com>
2025-11-11 16:45:37 -0300

    Refactor ThemeSwitcher component to remove unnecessary mounted state management

--- app/providers/UIProvider/ThemeSwitcher.tsx
+++ app/providers/UIProvider/ThemeSwitcher.tsx
@@ -3,18 +3,10 @@
 import "@theme-toggles/react/css/Classic.css";
 
 import { useTheme } from "next-themes";
-import { useEffect, useState } from "react";
 import { Classic } from "@theme-toggles/react";
 
 export function ThemeSwitcher() {
-  const [mounted, setMounted] = useState(false);
   const { theme, setTheme } = useTheme();
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!mounted) return null;
 
   return (
     <Classic
