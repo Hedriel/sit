419bcb516c76b60c15e14dffd864f941d58f7af4
Hernán Gonzalez <hernan.gonzalez.magliano@gmail.com>
2025-11-28 17:11:47 -0300

    - Linter auth.js

--- _auth/auth.ts
+++ _auth/auth.ts
@@ -5,11 +5,13 @@ import { redirect } from "next/navigation";
 const supabase = await createClient();
 
 export const isLoggedIn = async () => {
-  const { data: { session } } = await supabase.auth.getSession();
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
   return !!session;
 };
 
 export async function loginOut() {
-  const { error } = await supabase.auth.signOut();  
-    redirect("/sing-in");
+  const { error } = await supabase.auth.signOut();
+  redirect("/sing-in");
 }
