1ff79462607caabb381a645bd92f78e4bdece52d
Hernán Gonzalez <hernan.gonzalez.magliano@gmail.com>
2026-01-06 23:00:50 -0300

    - Rollbacked admin page restriction

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,13 +1,15 @@
 import { getUserProfile } from "@/lib/auth/user";
 import { getUsers } from "@/lib/data-access-layer/admin/users";
-import BreadCrumb from "./_components/BreadCrumb";
 import UsersTable from "./_components/UsersTable";
+
+import BreadCrumb from "./_components/BreadCrumb";
+
 import { redirectToHome } from "@/lib/utils";
 export default async function AdminPage() {
   const user = await getUserProfile();
-  // if (user?.role !== "admin") {
-  //   redirectToHome();
-  // }
+  if (user?.role !== "admin") {
+    redirectToHome();
+  }
   const { users } = await getUsers();
 
   return (
