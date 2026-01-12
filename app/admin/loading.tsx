bf35b2c037bcc359319225de9bd97f6d0c158ceb
Hedriel <hernan.gonzalez.magliano@gmail.com>
2026-01-12 13:50:43 -0300

    - Added loading skeleton to admin page

---
+++ app/admin/loading.tsx
@@ -0,0 +1,19 @@
+export default function LoadingAdminPage() {
+  return (
+    <div className="max-w-7xl mx-auto px-7">
+      <div className="mr-1 h-4 w-32 bg-gray-300 dark:bg-gray-800 animate-pulse rounded-xl my-4" />
+      <div className="mt-5 flex justify-between">
+        <div className="mr-1 h-10 w-96 bg-gray-300 dark:bg-gray-800 animate-pulse rounded-xl" />
+        <div className="mr-1 h-10 w-42 bg-gray-300 dark:bg-gray-800 animate-pulse rounded-xl" />
+      </div>
+
+      <div className="mr-1 h-4 w-32 bg-gray-300 dark:bg-gray-800 animate-pulse rounded-xl mt-5 mb-4" />
+
+      <div className="p-4 w-full  bg-gray-300 dark:bg-gray-800 animate-pulse rounded-xl ">
+        <div className="w-full h-10 bg-gray-200 dark:bg-gray-700 animate-pulse rounded-xl mb-2" />
+        <div className="w-full h-13 bg-gray-200 dark:bg-gray-700 animate-pulse rounded-xl mb-2" />
+        <div className="w-full h-13 bg-gray-200 dark:bg-gray-700 animate-pulse rounded-xl" />
+      </div>
+    </div>
+  );
+}
--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   // cacheComponents: true,
+  output: "standalone",
   /* config options here */
 };
 
 export default nextConfig;
+  
