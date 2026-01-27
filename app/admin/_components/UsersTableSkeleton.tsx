1c640ea4a6802d415c195603c5c7cad71d51e5a1
Hedriel <hernan.gonzalez.magliano@gmail.com>
2026-01-27 12:44:08 -0300

    - Fix skeleton width

--- app/admin/_components/UsersTableSkeleton.tsx
+++ app/admin/_components/UsersTableSkeleton.tsx
@@ -2,8 +2,8 @@ export default function UsersTableSkeleton() {
   return (
     <div className="mx-auto w-full max-w-7xl">
       <div className="flex justify-between">
-        <div className="mr-1 h-10 w-96 animate-pulse rounded-xl bg-gray-300 dark:bg-gray-800" />
-        <div className="mr-1 h-10 w-42 animate-pulse rounded-xl bg-gray-300 dark:bg-gray-800" />
+        <div className="mr-1 h-10 w-[377px] animate-pulse rounded-xl bg-gray-300 dark:bg-gray-800" />
+        <div className="h-10 w-42 animate-pulse rounded-xl bg-gray-300 dark:bg-gray-800" />
       </div>
 
       <div className="mt-5 mr-1 mb-4 h-4 w-32 animate-pulse rounded-xl bg-gray-300 dark:bg-gray-800" />
